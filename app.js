565bc0bc68236a289ab46b4fa4a2dfe27d42d92c
chrisallenarmbruster <chris@armbrustermail.com>
2021-02-06 11:38:54 -0600

    Setting Up Flash Routine

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const process = require("process");
 const lightArray = require("./config");
 
-for (let i = 0; i < lightArray.length; i++) {
-  lightArray[i].writeSync(1);
-}
-
 function allLightsOff() {
   for (let i = 0; i < lightArray.length; i++) {
     lightArray[i].writeSync(0);
+  }
+}
+
+function allLightsOn() {
+  for (let i = 0; i < lightArray.length; i++) {
+    lightArray[i].writeSync(1);
   }
 }
 
@@ -17,9 +19,12 @@ function sleep(ms) {
   });
 }
 
-async function loop() {
+async function flash() {
   while (true) {
-    await sleep(100);
+    allLightsOn();
+    await sleep(1000);
+    allLightsOff();
+    await sleep(1000);
   }
 }
 
@@ -28,4 +33,4 @@ process.on("SIGINT", () => {
   process.exit();
 });
 
-loop();
+flash();

c5816cf855a0350583cdda3b5f1257282d07c75a
chrisallenarmbruster <chris@armbrustermail.com>
2021-02-09 07:42:34 -0600

    Testing basic api get request.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Xmas-Lights up and running.  API listening.  Merry Christmas!");
 });
 
 module.exports = server;

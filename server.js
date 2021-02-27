11998a3ffb242aadf7265cd5f2916f99ba65afb9
chrisallenarmbruster <chris@armbrustermail.com>
2021-02-27 13:24:12 -0600

    Fixed Typo

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = express();
 
+app.use(express.static(__dirname + "/public"));
+
 server.get("/", (req, res) => {
   res.send("Xmas-Lights up and running.  API listening.  Merry Christmas!");
 });

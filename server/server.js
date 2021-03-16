76cc696431fc71fa6b5d7136c9c108272895907d
chrisallenarmbruster <chris@armbrustermail.com>
2021-03-16 10:19:57 -0500

    "Removed experimental code"

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const server = express();
+const bodyparser = require("body-parser");
 const path = require("path");
 
 server.use(express.static(path.join(__dirname, "../public")));
+
+server.use(bodyparser.urlencoded({ extended: false }));
+server.use(bodyparser.json());
 
 server.get("/", (req, res) => {
   res.send("Xmas-Lights up and running.  API listening.  Merry Christmas!");
